be84b6a2a7eed10457fe51df5cf69882b2832230
Michał Brusiło <mibru@interia.pl>
2018-06-19 19:24:35 +0200

    Update App.js and add UserForm.js

--- client/App.js
+++ client/App.js
@@ -29,6 +29,17 @@ class App extends Component {
   chatUpdate(users) {
   this.setState({users});
  }
+
+ handleMessageSubmit(message) {
+  const messages = [message, ...this.state.messages];
+  this.setState({messages});
+  socket.emit('message', message);
+ }
+
+ handleUserSubmit(name) {
+  this.setState({name});
+  socket.emit('join', name);
+ }
  
   render() {
     return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
